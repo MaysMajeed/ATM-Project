bedb3e0e3aa151e86b2d99c44d5878917093be21
hamdon <hamdongunner@gmail.com>
2020-05-08 22:27:53 +0300

    😋 V1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const Joi = require("joi");
 const hello = require("./middlewares/hello");
-const router = require("./routes/v1");
+const routerV1 = require("./routes/v1");
 const mongoose = require("mongoose");
 
 app.use(express.json());
@@ -14,7 +14,13 @@ mongoose
   .then(() => console.log("connected to the database ^_^"))
   .catch((err) => console.error("Connection Faild!"));
 
-app.use(router);
+app.use("/v1", routerV1);
+
+app.use("/", (req, res) => {
+  return res.status(400).send({
+    msg: "404 Not Found",
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening to ${port}...`));
